b434df3978c00e5faf8f4c169b283548b8410b93
kuroeita <kurokawa@nana-music.com>
2019-09-05 18:27:16 +0900

    詳細画面/編集を実装

--- tasklist/tasklist/AddController.swift
+++ tasklist/tasklist/AddController.swift
@@ -19,11 +19,58 @@ class AddController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+//        self.saveButton.isEnabled = false
+//        //文字が入力されるまでSave ボタンを無効←privateメソッドで書く
+        
+        if let edittitle = self.taitle {
+            //prepare メソッドから値が渡されていたら taitle に値がセットされている
+            self.todoTitle.text = edittitle
+            //値がセットされていたらedittitleに値を渡す
+            self.navigationItem.title = "Details"
+            //値がセットされていたらタイトルを変更
+            //ボタンも変更したい
+        }
+        self.saveButtinState()
+        //TextField の値を設定した後に呼ぶ
     }
     
-
+    private func saveButtinState() {
+        //saveButtonの中身がなかった時に無効にする処理
+        let title = self.todoTitle.text ?? ""
+        self.saveButton.isEnabled = !title.isEmpty
+            }
+    
+    
+     @IBAction func titleTextFieldChanged(_ sender: Any) {
+        //textField に入力された値を検出
+//        let title = self.todoTitle.text ?? "" ←praivateメソッドで処理
+//        //nil だったら空文字にする
+//        self.saveButton.isEnabled = !title.isEmpty
+//        //空文字じゃなかったら有効
+        self.saveButtinState()
+        //saveButtonの中身がなかった時に無効にする処理を呼ぶ
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        if self.presentingViewController is UINavigationController {
+            //モーダル閉じるととセグエを辿って戻るがあるので、条件分岐
+            //presentingViewController に今の ViewController を表示している ViewController がセットされている
+            
+            self.dismiss(animated: true, completion: nil)
+            //キャンセルを押した時にモーダルを閉じる処理
+            //アニメーションは true、終わった後の処理は特にないので nil
+        } else {
+            self.navigationController?.popViewController(animated: true)
+            //モーダルでは無く、showしたsegueを逆に戻る時の処理
+            //popViewControllerという命令を使う
+        }
+        
+    }
+    
+    
+    
     
     // MARK: - Navigation
 
@@ -34,7 +81,7 @@ class AddController: UIViewController {
         guard let button = sender as? UIBarButtonItem, button === self.saveButton else {
             return
         }
-        self.title = self.todoTitle.text ?? ""
+        self.taitle = self.todoTitle.text ?? ""
         //チェックがOKだった箇所の処理
     }
     
